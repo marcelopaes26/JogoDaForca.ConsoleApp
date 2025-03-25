c1a4bfbe4e45337ecd1102823f192aa76fefe561
marcelopaes26 <marcelopaesfarias@gmail.com>
2025-03-25 19:27:38 -0300

    Implementada a estrutura básica do projeto

---
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace JogoDaForca.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("\tJogo da Forca");
+                Console.WriteLine("----------------------------");
+            }
+
+            Console.Write("Digite uma palavra: ");
+            char chute = Console.ReadLine()[0];
+        }
+    }
+}
