73a8cf49d14dbebec6e2437fb34aa519d10c0239
marcelopaes26 <marcelopaesfarias@gmail.com>
2025-03-25 20:55:06 -0300

    Implementadas as condições de vitória e derrota

--- JogoDaForca.ConsoleApp/Program.cs
+++ JogoDaForca.ConsoleApp/Program.cs
@@ -4,10 +4,10 @@
     {
         static void Main(string[] args)
         {
-            while(true)
+            while (true)
             {
 
-                string palavraSecreta = "MELANCIA";
+                string palavraSecreta = "UVA";
 
                 char[] letrasEncontradas = new char[palavraSecreta.Length];
 
@@ -16,22 +16,69 @@
                     letrasEncontradas[i] = '_';
                 }
 
-                string dicaDaPalavra = String.Join(" ", letrasEncontradas);
+                int quantidadeErros = 0;
+                bool jogadorEnforcou = false, jogadorAcertou = false;
 
-                Console.Clear();
-                Console.WriteLine("---------------------------------------");
-                Console.WriteLine("\tJogo da Forca");
-                Console.WriteLine("---------------------------------------");
-                Console.WriteLine($"Palavra secreta: {dicaDaPalavra}");
-                Console.WriteLine("---------------------------------------");
+                do
+                {
+
+                    string dicaDaPalavra = String.Join(" ", letrasEncontradas);
+
+                    Console.Clear();
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("\tJogo da Forca");
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine($"Palavra secreta: {dicaDaPalavra}");
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine($"Quantidade de erros: {quantidadeErros}");
+                    Console.WriteLine("---------------------------------------");
+
+                    Console.Write("Digite uma letra: ");
+                    char chute = Console.ReadLine()[0];
+
+                    bool letraFoiEncontrada = false;
+
+                    for (int i = 0; i < palavraSecreta.Length; i++)
+                    {
+                        char letraAtual = palavraSecreta[i];
+
+                        if (chute == letraAtual)
+                        {
+                            letrasEncontradas[i] = letraAtual;
+                            letraFoiEncontrada = true;
+                        }
+
+                    }
+
+                    if (!letraFoiEncontrada)
+                    {
+                        quantidadeErros++;
+                    }
+
+                    dicaDaPalavra = String.Join("", letrasEncontradas);
+
+                    jogadorAcertou = dicaDaPalavra == palavraSecreta;
+                    jogadorEnforcou = quantidadeErros > 5;
+
+                    if (jogadorAcertou)
+                    {
+                        Console.WriteLine("---------------------------------------");
+                        Console.WriteLine("Você acertou a palavra secreta!");
+                        Console.WriteLine("---------------------------------------");
+                    }
+                    else if (jogadorEnforcou)
+                    {
+                        Console.WriteLine("---------------------------------------");
+                        Console.WriteLine($"Que azar, tente novamente! \nA palavra era: {palavraSecreta}");
+                        Console.WriteLine("---------------------------------------");
+                    }
+
+                    Console.ReadLine();
+
+                } while (jogadorAcertou == false && jogadorEnforcou == false);
+
             }
 
-            Console.Write("Digite uma letra: ");
-            char chute = Console.ReadLine()[0];
-
-            Console.WriteLine(chute);
-
-            Console.ReadLine();
         }
     }
 }
